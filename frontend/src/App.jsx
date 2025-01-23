534a264d7cd4d3a7b610da7aafdad438b03c4800
godsky <gautheyelohin@gmail.com>
2025-01-23 21:56:03 +0100

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+
+import Button from './conpoments/Buttons/Button'
+import Input from './conpoments/Input/Input'
+import Container from './conpoments/Container/Container'
+import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
+import Email from './pages/Update/Email'
+import Password from './pages/Update/Password'
+import Verification from './pages/OptVerification/Verification'
+import Sidebar from './conpoments/Sidebar/Sidebar'
+
+function App() {
+
+  return (
+    <>
+      <Container>
+
+      </Container>
+    </>
+  )
+}
+
+export default App
